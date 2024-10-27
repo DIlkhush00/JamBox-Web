f83052a44cba34ce6c379b9e77727b0edad9982f
Dilkhush <dilkhushpurohit01@gmail.com>
2024-10-28 00:12:29 +0530

    setup starter template

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+function App () {
+  const [count, setCount] = React.useState(0)
+
+  return (
+    <>
+      <p>{ count }</p>
+      <button onClick={()=>setCount(count + 1)}>Increase</button>
+      <button onClick={()=>setCount(count - 1)}>Decrease</button>
+    </>
+
+  )
+}
+
+export default App
